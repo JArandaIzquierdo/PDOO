bfd28d427943549c6e5bb8b9ff445cca6ea87e4d
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-10-14 20:00:18 +0200

    Agregados dos monstruos

--- Napakalaki/src/napakalaki/Prize.java
+++ Napakalaki/src/napakalaki/Prize.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* Clase Prize lo utilizaremos para el premio de los monstruos*/
+
 package napakalaki;
 
 /**
--- Napakalaki/src/napakalaki/PruebaNapakalaki2.java
+++ Napakalaki/src/napakalaki/PruebaNapakalaki2.java
@@ -6,6 +6,7 @@
 package napakalaki;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  *
@@ -16,11 +17,45 @@ public class PruebaNapakalaki2 {
     /**
      * @param args the command line arguments
      */
+    
+     // Declaracion de variables
+        static BadConsequence badConsequence; //Variable para el mal rollo 
+        static Prize prize; // Premio 
+    
+    
     public static void main(String[] args) {
         
-        System.out.println ("Empezamos la ejecución del programa");
         
+        // Array para los monstruos
         ArrayList<Monster> monstruos = new ArrayList();
+        
+        
+        /* Creacion de los monstruos */
+        
+        // Creacion de "El rey de rosa"
+        
+        //Mal rollo 
+        badConsequence = new BadConsequence ("Pierder 5 niveles y tres tesoros visibles",5,3,0);
+        
+        //Premio
+        prize= new Prize (4,2);
+        
+        //Añadimos el monstruo
+        monstruos.add(new Monster("El rey de rosa",13,badConsequence,prize));
+        
+        //Creacion "Angeles de la noche ibicenca"
+        
+        //Mal rollo
+        badConsequence = new BadConsequence("Te atrapan para llevarte de fiesta y te dejan caer en mitad del vuelo. Descarta una "
+                + "mano visible y una mano oculta", 0, new ArrayList(Arrays.asList(TreasureKind.ONEHAND)), 
+                new ArrayList(Arrays.asList(TreasureKind.ONEHAND)));
+         //Premio
+        prize = new Prize(4, 1);
+        
+        //Añadimos el monstruo
+        monstruos.add(new Monster("Angel de la noche ibicenca",14,badConsequence,prize));
+        
+        
     
     }
 }
