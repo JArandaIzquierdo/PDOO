891124dc2de8fb84d388620a724594c6af95dd1a
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-10-25 12:34:28 +0100

    Hecha la clase Prize en Ruby

--- NapakalakiRubyJavi/lib/prize.rb
+++ NapakalakiRubyJavi/lib/prize.rb
@@ -1,9 +1,14 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
+#Clase para el premio
 class Prize
-  def initialize
-    
+  def initialize (treasures,level)
+    @treasures=treasures
+    @level=level
+  end
+
+
+  attr_reader :treasures, :level
+  
+  def to_s
+    "Tesoros ganados: #{@treasures} \n Niveles ganados: #{@levels}"
   end
 end
