3f9b0ae3f5fd29a0fa30f67b801814fe86c7f77f
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-10-25 20:37:25 +0100

    Termina la clase monster en Ruby

--- NapakalakiRubyJavi/lib/monster.rb
+++ NapakalakiRubyJavi/lib/monster.rb
@@ -15,5 +15,11 @@ class Monster
 # Consulores basicos
 attr_accesor:name
 attr_accesor:combatLEvel
+attr_accesor:prize
+attr_accesor:badConsequenze
 
+end
+
+def to_s
+  "Nombre del monstruo: #{name} \nNivel de combate: #{level}"
 end
