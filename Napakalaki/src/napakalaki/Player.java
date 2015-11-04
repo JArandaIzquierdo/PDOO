31e1845172e23f6972a27e842a62233111659872
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-11-04 19:36:57 +0100

    Creadas todas las variables de la clase Player y varios metodos

--- Napakalaki/src/napakalaki/Player.java
+++ Napakalaki/src/napakalaki/Player.java
@@ -5,5 +5,49 @@ package napakalaki;
  * @author JaviAir
  */
 public class Player {
+    static final int MAXLEVEL=10;
+    private String name;
+    private int level;
+    private boolean dead=true;
+    private boolean canISteal=true;
+    
+
+
+    public Player (String name){
+    
+
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+    private void bringToLife(){
+    
+    }
+    
+    private int getCombatLevel(){
+        return 0;
+    }
+    
+    private void incrementLevels(int l){
+    
+    }
+    
+    private void decrementLevels(int l){
+    
+    }
+    
+    private void setPendingBadConsequence(BadConsequence b){
+    
+    }
+    
+    private void applyPrize(Monster m){
+    
+    }
+    
+    private void applyBadConsequence(Monster m){
+    
+    }
     
 }
