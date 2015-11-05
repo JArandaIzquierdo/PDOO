d29697962dfb9c11c6338d7af0daec7a355fbbb4
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-11-05 18:33:53 +0100

    Declaradas las variables de la clase Player en Ruby

--- Napakalaki-Ruby/lib/player.rb
+++ Napakalaki-Ruby/lib/player.rb
@@ -1,7 +1,12 @@
 # Author: Javier Aranda
 
 class Player
-  def initialize(nom)
-    
+  
+  CONST_MAXLEVEL = 10
+  def initialize(nom,nivel,muerte=true,robar=true)
+    @nombre=nom
+    @level=nivel
+    @dead=muerte
+    @canISteal=robar
   end
 end
