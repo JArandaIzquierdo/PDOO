759b8e2081d13d25a7d92809385b90a8a88f62ed
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-11-05 19:14:56 +0100

    Creados los metodos publicos de la clase player en ruby

--- Napakalaki-Ruby/lib/player.rb
+++ Napakalaki-Ruby/lib/player.rb
@@ -9,4 +9,92 @@ class Player
     @dead=muerte
     @canISteal=robar
   end
+  
+  # ------------ Metodos publicos --------------------
+  
+  # Constructor con el parametro nombre
+  def Player (name)
+    
+  end
+
+  # Metodo getName
+  def getName ()
+    
+  end
+  
+  # Metodo isDead
+  def isDead()
+    
+  end
+  
+  # Metodo getHiddenTreasures
+  def getHiddenTreasures()
+    
+  end
+  
+  # Metodo getVisibleTreasures
+  def getVisibleTreasures()
+    
+  end
+  
+  # Metodo combat
+  def combat (m)
+    
+  end
+  
+  # Metodo makeTreasureVisible
+  def makeTreasureVisible (t)
+    
+  end
+  
+  # Metodo discardVisibleTreasure
+  def discardVisibleTreasure(t)
+    
+  end
+  
+  # Metodo discardHiddenTreasure
+  def discardHiddenTreasure(t)
+    
+  end
+  
+  # Metodo validState
+  def validState()
+    
+  end
+  
+  # Metodo initTreaures
+  def initTreasures()
+    
+  end
+  
+  # Metodo getLevels
+  def getLevels ()
+    
+  end
+  
+  # Metodo stealTreasure
+  def steadTreasure()
+    
+  end
+  
+  # Metodo setEnemy
+  def setEnemy(enemy)
+    
+  end
+  
+  # Metodo canISteal
+  def canISteal()
+    
+  end
+  
+  # Metodo discardAllTreasures
+  def discardAllTreasures()
+    
+  end
+  
+  # --------------- Metodos Privados ----------
+  private
+  
+  
+  
 end
