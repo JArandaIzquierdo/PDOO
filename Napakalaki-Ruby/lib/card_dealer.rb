4830534c4b4657785084489506e587c6cdad390b
JArandaIzquierdo <j.arandaizquierdo@gmail.com>
2015-11-06 12:11:22 +0100

    Creada la clase CardDealer y creados sus metodos publicos

---
+++ Napakalaki-Ruby/lib/card_dealer.rb
@@ -0,0 +1,37 @@
+# Autor Javier Aranda
+
+require "singleton"
+
+class CardDealer
+  def initialize
+    @intance=CardDealer.new
+    
+  end
+  
+  # ----------------- Metodos Publicos --------------------
+  
+  # Metodo nextTreasure
+  def nextTreasure()
+    
+  end
+  
+  # Metodo nextMonster
+  def nextMonster()
+    
+  end
+  
+  # Metodo giveTreasureBack
+  def giveTreasureBack(t)
+    
+  end
+  
+  # Metodo giveMonsterBack
+  def giveMonsterBack(m)
+    
+  end
+  
+  # Metodo 
+  def initCards()
+    
+  end
+end
