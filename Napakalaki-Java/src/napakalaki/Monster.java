6dc9654a99289e3602291246b0ba64fa5f45962b
Javier Aranda <j.arandaizquierdo@gmail.com>
2015-11-29 00:22:05 +0100

    Revisada clase Monster.java, eliminados metodod que no estaban el el diagrama de clases

--- Napakalaki-Java/src/napakalaki/Monster.java
+++ Napakalaki-Java/src/napakalaki/Monster.java
@@ -2,40 +2,23 @@ package napakalaki;
 
 /**
  *
- * @author carlos
+ * @author Javier Aranda Izquierdo
  */
 public class Monster {
     // Atributos privados
     private String name;
     private int combatLevel;
-    private BadConsequence bc;
-    private Prize price;
+    private BadConsequence badConsequnece;
+    private Prize prize;
     
     //Constructor
-    Monster(String name, int level, BadConsequence bc, Prize price){
-        setName(name);
-        setCombatLevel(level);
-        setBadConsequence(bc);
-        setPrize(price);
+    Monster(String n, int l, BadConsequence b, Prize p){
+        name=n;
+        combatLevel=l;
+        badConsequnece=b;
+        prize=p;
     }
-    
-    // Modificadores
-    private void setName(String name_1){
-        name = name_1;
-    }
-    
-    private void setCombatLevel(int combat_level){
-        combatLevel = combat_level;
-    }
-    
-    private void setBadConsequence(BadConsequence Bad_c){
-        bc = Bad_c;
-    }
-    
-    private void setPrize(Prize price_1){
-        price = price_1;
-    }
-    
+        
     //Consultores
     public String getName(){  //Consultor de Name
         return name;
@@ -46,52 +29,15 @@ public class Monster {
     }
     
     public BadConsequence getBadConsequence(){  //Consultor de BadConsequence
-        return bc;
-    }
-    
-    public Prize getPrize(){  //Consultor de Prize
-        return price;
-    }
-    
-    // Método Consulta monstruos con niveles igual o superior a 10
-    public boolean LevelExceeding10(){
-        if(combatLevel>=10)
-            return true;
-        else
-            return false;
-    }
-    
-    // Método Consulta monstruos cuyo mal rollo solo implique pérdida de niveles
-    public boolean OnlyLevelsBadConsequence(){
-        if((bc.getLevels()>0) && (bc.getNVisibleTreasures()==0) && (bc.getNHiddenTreasures()==0))
-            return true;
-        else
-            return false;
-    }
-    
-    // Método Consulta monstruos cuyo buen rollo tenga un nivel mayor que 1
-    public boolean PrizeLevelsExceeding1(){
-        if(price.getLevel()>1)
-            return true;
-        else
-            return false;
-    }
-    
-    /* Método Consulta monstruos cuyo mal rollo implique perdida de un determinado
-    de tesoro ya sea visible u oculto */
-    public boolean LoseSpecificTreasures(){
-        if(bc.TieneTesoros())
-            return true;
-        else
-            return false;
+        return badConsequnece;
     }
     
     public int getLevelsGained(){
-        return price.getLevel();
+        return 0;
     }
     
     public int getTreasuresGained(){
-        return price.getTreasure();
+        return 0;
     }
     
     // Método toString
