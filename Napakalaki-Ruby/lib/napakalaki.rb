5a9e27a270c94a66d4ec460fa35bc9e59d2ac6e5
Javier Aranda <j.arandaizquierdo@gmail.com>
2015-11-30 22:53:54 +0100

    Revisada y modificada la clase napakalaki.rb

--- Napakalaki-Ruby/lib/napakalaki.rb
+++ Napakalaki-Ruby/lib/napakalaki.rb
@@ -1,70 +1,75 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
-require "singleton"
-
-class Napakalaki
+# Javier Aranda Izquierdo
+module NapakalakiGame
   
-  include Singleton
-  
-  def initialize(resultado)
-    @result=resultado
+  require "singleton"
+
+  class Napakalaki
+
+    include Singleton
+
+    def initialize(resultado,currentMonster,dealer,currentPlayer,players)
+      @result=resultado
+      @currentMonster=currentMonster
+      @dealer=dealer
+      @currentPlayer=currentPlayer
+      @players=players
+
+    end
+
+      private def Napakalaki
+
+      end
+
+      private def initPlayers(names)
+
+      end
+
+      private def nextPlayer
+
+      end
+
+      private def nextTurnAllowed
+
+      end
+
+      private def setEnemies
+
+      end
+
+      def developCombat
+
+      end
+
+      def discardVisibleTreasures(treasures)
+
+      end
+
+      def discardHiddenTreasures(treasures)
+
+      end
+
+      def makeTreasuresVisible(treasures)
+
+      end
+
+      def initGame(players)
+
+      end
+
+      def getCurrentPlayer
+
+      end
+
+      def getCurrentMonster
+
+      end
+
+      def nextTurn
+
+      end
+
+      def endOfGame(result)
+
+      end
   end
-    
-    def Napakalaki
-    
-    end
-    
-    def initPlayers(names)
-    
-    end
-    
-    def nextPlayer
-      
-    end
-    
-    def nextTurnAllowed
-      
-    end
-    
-    def setEnemies
-      
-    end
-    
-    def developCombat
-      
-    end
-    
-    def discardVisibleTreasures(treasures)
-      
-    end
-    
-    def discardHiddenTreasures(treasures)
-      
-    end
-    
-    def makeTreasuresVisible(treasures)
-      
-    end
-    
-    def initGame(players)
-      
-    end
-    
-    def getCurrentPlayer
-      
-    end
-    
-    def getCurrentMonster
-      
-    end
-    
-    def nextTurn
-      
-    end
-    
-    def endOfGame(result)
-      
-    end
 end
