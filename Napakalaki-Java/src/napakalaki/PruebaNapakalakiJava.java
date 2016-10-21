98578eb828aafe58bf1e8f86a6c9d8c1304c53af
Javier Aranda <j.arandaizquierdo@gmail.com>
2016-10-21 18:06:45 +0200

    Creado de nuevo el proyecto en Java

--- Napakalaki-Java/src/napakalaki/PruebaNapakalakiJava.java
+++ Napakalaki-Java/src/napakalaki/PruebaNapakalakiJava.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package napakalaki.java;
+package napakalaki;
 
 /**
  *
  * @author JaviAir
  */
-public class NapakalakiJava {
+public class PruebaNapakalakiJava {
 
     /**
      * @param args the command line arguments
