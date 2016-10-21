9cfc2f8ea2421aa8afc003e648369adc3c84bad0
Javier Aranda <j.arandaizquierdo@gmail.com>
2016-10-21 19:24:42 +0200

    Terminada la clase Monster

---
+++ Napakalaki-Java/src/napakalaki/Monster.java
@@ -0,0 +1,39 @@
+package napakalaki;
+
+/**
+ *
+ * @author Javier Aranda
+ */
+public class Monster {
+    
+    //Attrb
+    private String name;
+    private int combatLevel;
+    private BadConsequence badConsequence;
+    private Prize prize;
+    
+    //Constrc
+    public Monster(String name, int level, BadConsequence bc, Prize prize){
+        this.name=name;
+        combatLevel=level;
+        badConsequence=bc;
+        this.prize=prize;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public int getCombatLevel(){
+        return combatLevel;
+    }
+    
+    public BadConsequence getBadConsequence(){
+        return badConsequence;
+    }
+    
+    public Prize getPrize(){
+        return prize;
+    }
+    
+}
