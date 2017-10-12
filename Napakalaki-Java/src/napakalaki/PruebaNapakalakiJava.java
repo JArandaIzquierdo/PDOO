687a9a02285d1cc2c953e5e4bd03e26d63cfa05e
Javier Aranda <j.arandaizquierdo@gmail.com>
2017-10-12 10:18:55 +0200

    Creado poryecto QytetetJava

--- Napakalaki-Java/src/napakalaki/PruebaNapakalakiJava.java
+++ Napakalaki-Java/src/napakalaki/PruebaNapakalakiJava.java
@@ -1,5 +1,7 @@
 package napakalaki;
 
+import java.util.ArrayList;
+
 
 /**
  *
@@ -7,26 +9,35 @@ package napakalaki;
  */
 public class PruebaNapakalakiJava {
 
-    /**
-     * @param args the command line arguments
-     */
+    static ArrayList<Monster> monstruos = new ArrayList();
+        
+    
     public static void main(String[] args) {
+        
         
         System.out.println("Bienvenido a Napakalaki");
         
+        // Variables
+        BadConsequence badConsequence;
+        Prize prize;
+        Monster monstruo;
+        
+        // 3 Byakhees de bonanza
+        
+        badConsequence = new BadConsequence("Pierdes tu armadura visible y otra oculta",)
         // Creacion de un objeto de cada tipo
-        BadConsequence malRollo = new BadConsequence("El peor mal rollo",5,2,2);
-        Prize buenRollo = new Prize(4,5);
-        Monster monstruo = new Monster("Joker",2,malRollo, buenRollo);
-        
-        System.out.println("El mal rollo es: ");
-        System.out.println(malRollo);
-        
-        System.out.println("El buen rollo es: ");
-        System.out.println(buenRollo);
-        
-        System.out.println("El monstruo es: ");
-        System.out.println(monstruo);
+//        BadConsequence malRollo = new BadConsequence("El peor mal rollo",5,2,2);
+//        Prize buenRollo = new Prize(4,5);
+//        Monster monstruo = new Monster("Joker",2,malRollo, buenRollo);
+//
+//        System.out.println("El mal rollo es: ");
+//        System.out.println(malRollo);
+//
+//        System.out.println("El buen rollo es: ");
+//        System.out.println(buenRollo);
+//
+//        System.out.println("El monstruo es: ");
+//        System.out.println(monstruo);
     }
     
 }
