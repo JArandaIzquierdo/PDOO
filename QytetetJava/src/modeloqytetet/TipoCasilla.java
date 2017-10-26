d27bc3179d1d8bf3946c809f43c37b5798c5b3e0
Javier Aranda <j.arandaizquierdo@gmail.com>
2017-10-26 16:55:41 +0200

    Creado el enumero para el TipoCasilla

---
+++ QytetetJava/src/modeloqytetet/TipoCasilla.java
@@ -0,0 +1,12 @@
+package modeloqytetet;
+
+/**
+ *
+ * @author Javier Aranda Izquierdo GitHub:JArandaIzquierdo
+ * 
+ * Este enumero se compone de los tipos de casillas que existen en el juego
+ */
+public enum TipoCasilla {
+    SALIDA, CALLE, SORPRESA, CARCEL, JUEZ, IMPUESTO, PARKING;
+    
+}
